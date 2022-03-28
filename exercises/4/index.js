2d989692e817f553f1ee9e5925c1be723832ba43
Lauren <lhanna7@ford.com>
2022-03-28 13:48:51 -0700

    finish thru 5

---
+++ exercises/4/index.js
@@ -0,0 +1,2 @@
+const listItems = document.querySelectorAll("li")
+listItems.forEach(li => li.className = "corgi")
---
+++ exercises/5/index.js
@@ -0,0 +1,3 @@
+const changeLis = document.querySelectorAll("li")
+changeLis[0].textContent = "Pembroke"
+changeLis[1].textContent = "Cardigan"
