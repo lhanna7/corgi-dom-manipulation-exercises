24df0a97b1223aae1a8fc4c0d29c34f329699a79
Lauren <lhanna7@ford.com>
2022-03-29 09:29:37 -0700

    answer thru 17

---
+++ exercises/12/index.js
@@ -0,0 +1,6 @@
+const div = document.querySelector("div.queens")
+const ul = document.createElement("ul")
+const li = document.createElement("li")
+li.textContent = "Kiki"
+ul.append(li)
+div.append(ul)
